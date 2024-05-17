2b0d65a460d9f5c00708913c9cbe3311d34aabbd
sandro1245 <142908554+sandro1245@users.noreply.github.com>
2024-05-17 22:35:36 +0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='moderncarousel',
+   version='1.0',
+   description='Kivy component',
+   author='moderncarousel',
+   author_email='@.example',
+   packages=['moderncarousel'],
+   install_requires=['kivy'] 
+)
